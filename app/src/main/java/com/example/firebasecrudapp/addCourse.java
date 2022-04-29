af3065a0cf2cb43155e38e460b736dec5edc27e3
shahriar021 <shahriarc19@gmail.com>
2022-04-29 22:51:35 +0600

    firebase

---
+++ app/src/main/java/com/example/firebasecrudapp/addCourse.java
@@ -0,0 +1,14 @@
+package com.example.firebasecrudapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+    }
+}
